f0b02c38a7fc33243f4228601ad6877764d8064f
Achref <medachrefg@gmail.com>
2022-09-14 13:40:19 +0100

    init our porject & adding dependencies&working on the navBar

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { Stack } from '@mui/material';
+import './NavBar.css';
+import Logo from '../assets/images/Logo.png';
+const NavBar = () => {
+	return (
+		<Stack
+			direction='row'
+			justifyContent='space-around'
+			sx={{
+				gap: { sm: '122px', xs: '40px' },
+				mt: { sm: '32px', xs: '20px' },
+				justifyContent: 'none',
+			}}
+			px='20px'>
+			<Link to='/'>
+				<img className='navBar-img' src={Logo} alt='logo' />
+			</Link>
+			<Stack direction='row' gap='40px' fontSize='24px' alignItems='flex-end'>
+				<Link to='/' className='navBarLinks'>
+					Home
+				</Link>
+				<a className='navBar-ancher' href='#exercices'>
+					Exercices
+				</a>
+			</Stack>
+		</Stack>
+	);
+};
+
+export default NavBar;
---
+++ src/pages/ExerciceDetails.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const ExerciceDetails = () => {
+	return <div>ExerciceDetails</div>;
+};
+
+export default ExerciceDetails;
